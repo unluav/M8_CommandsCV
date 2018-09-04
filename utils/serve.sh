1996b05c70126ac01fed68cb89c0a3fe94159b20
Jake Koperski <wijakoperski@gmail.com>
2018-09-04 10:05:33 -0500

    Added ffmpeg serve command to source control

---
+++ utils/serve.sh
@@ -0,0 +1,2 @@
+# ffserver & ffmpeg -s 640x480 -r 20 -f video4linux2 -i /dev/video0 http://localhost:8090/feed.ffm
+ffserver & ffmpeg -s 640x480 -r 20 -i /uav_rsc/arena-center2017.webm http://localhost:8090/file.webm
